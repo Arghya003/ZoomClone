f90605605ef65d41cfb1b578e8d4e941512b52e3
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2023-02-27 22:37:40 +0530

    Add files via upload

---
+++ plan.md
@@ -0,0 +1,12 @@
+# plan of action
+
+
+- Initialize our nodejs project **DONE**
+- Initialize our first view  **DONE**
+- Create a room id  **DONE**
+- Add the ability to view our own video
+- Add the ability to allow others stream their video
+- Add styling
+- Add the ability to create message
+-Add mute button
+- Add stop video button
---
+++ server.js
@@ -0,0 +1,31 @@
+const express= require("express");
+const app =express();
+
+const server=require('http').Server(app)
+const {v4: uuidv4}=require('uuid');
+app.set("view engine",'ejs');
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+   res.redirect(`/${uuidv4()}`)
+
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId:req.params.room})
+})
+
+
+
+
+
+
+
+
+
+
+const PORT=5000|| process.env.PORT;
+server.listen(PORT,()=>{
+    console.log(`server running on ${PORT}`)
+})
